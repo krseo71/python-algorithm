a503846c7b6ead81b0587aae71591a971ee63876
krseo71 <tjrudfhr123@naver.com>
2025-01-01 14:33:01 +0900

    힙 insert, delete 구현

---
+++ 4st_week/04_01_max_heap_insert.py
@@ -0,0 +1,28 @@
+class MaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    def insert(self, value):
+        # 1. 원소를 맨 끝에 추가
+        self.items.append(value)
+
+        cur_index = len(self.items) - 1
+        while cur_index != 1: # 1인 경우에는 root node라 더 비교할게 없음. 올라갈 일이 없음.
+            # 2. 부모 노드랑 비교해서 내가 더 크다면 위치를 바꾼다.
+            parent_index = cur_index // 2
+
+            if self.items[cur_index] > self.items[parent_index]:
+                self.items[cur_index], self.items[parent_index] = self.items[parent_index], self.items[cur_index]
+                cur_index = parent_index
+            else:
+                break
+
+        return
+
+
+max_heap = MaxHeap()
+max_heap.insert(3)
+max_heap.insert(4)
+max_heap.insert(2)
+max_heap.insert(9)
+print(max_heap.items)  # [None, 9, 4, 2, 3] 가 출력되어야 합니다!
